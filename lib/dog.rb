9373738d8044fc15909e932003d9fff16131db54
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-11 17:55:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
     SQL
     
     dog = DB[:conn].execute(sql, name, breed)
-    if dog.empty?
+    if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
